a1b6e25bf597182a2dbaf0fbf5cc844885f9c2d4
techy-geek <anuragsharma.nits@gmail.com>
2025-07-03 19:28:43 +0530

    frontend added..

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(() => {
     const getUser = async () => {
       await axios
-        .get("https://authfolio.netlify.app/api/v1/user/me", { withCredentials: true })
+        .get("https://authfolio.onrender.com/api/v1/user/me", { withCredentials: true })
         .then((res) => {
           setUser(res.data.user);
           setIsAuthenticated(true);
